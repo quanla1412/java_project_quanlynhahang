c6511c218d8e720bb82ca9760cb6b666e796a524
Lê Anh Quân <91645249+tinydead@users.noreply.github.com>
2023-03-26 12:27:32 +0700

    First created

---
+++ src/main/java/com/mycompany/quanlynhahang/QuanLyNhaHang.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.quanlynhahang;
+
+/**
+ *
+ * @author LeAnhQuan
+ */
+public class QuanLyNhaHang {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
