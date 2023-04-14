5255b5f255c81fab2b6205d9a2489fd34022fc81
Đinh Nhật Tân <dinhnhattan23@gmail.com>
2023-04-15 01:48:33 +0700

    done thêm sửa xoá chức vụ, nhân viên, chưa tìm kiếm, import, export

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -11,8 +11,14 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        TrangChu_GUI monAn_GUI = new TrangChu_GUI();
-        monAn_GUI.setVisible(true);
+//        QuanLyLoaiBanVaBan_GUI monAn_GUI = new QuanLyLoaiBanVaBan_GUI();
+//        monAn_GUI.setVisible(true);
+//        
+//        QuanLyChucVu_GUI monAn_GUI = new QuanLyChucVu_GUI();
+//        monAn_GUI.setVisible(true);
+
+          QuanLyNhanVien_GUI monAn_GUI = new QuanLyNhanVien_GUI();
+          monAn_GUI.setVisible(true);
     }
     
 }
